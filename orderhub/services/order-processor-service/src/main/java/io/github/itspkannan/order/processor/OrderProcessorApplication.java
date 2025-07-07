32f85f7a4ed8b0a36c13b329183aa467d7dbf2b6
Priyesh Kannan <10197527+PriyeshKannan@users.noreply.github.com>
2025-07-06 23:22:45 -0400

    Fixed order processor service startup

--- orderhub/services/order-processor-service/src/main/java/io/github/itspkannan/order/processor/OrderProcessorApplication.java
+++ orderhub/services/order-processor-service/src/main/java/io/github/itspkannan/order/processor/OrderProcessorApplication.java
@@ -1,8 +1,12 @@
 package io.github.itspkannan.order.processor;
 
+import io.github.itspkannan.aws.sqs.SqsListener;
+import io.github.itspkannan.order.eventhandler.EventRouter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication(
@@ -10,5 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderProcessorApplication {
   public static void main(String[] args) {
     SpringApplication.run(OrderProcessorApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner startListener(SqsListener sqsListener, EventRouter eventRouter) {
+    return args -> {
+      log.info("Starting SQS listener with message handler...");
+      sqsListener.start(eventRouter::route);
+    };
   }
 }
